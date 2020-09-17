9c72b3059a6d6978b4181eb9550958375c16808a
U-C-S <ucs3721@outlook.com>
2020-09-17 12:18:52 +0530

    v0.3 - Added a Menu Color effect which changes the background color based on mouse position

--- Something/MainWindow.xaml.cs
+++ Something/MainWindow.xaml.cs
@@ -9,9 +9,13 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Forms;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Markup;
 
 namespace Something
 {
@@ -24,7 +28,6 @@ namespace Something
         {
             InitializeComponent();
         }
-        branch br = new branch();
         private void TheStart(object sender, RoutedEventArgs e)
         {
             OpeningScreen.Visibility = Visibility.Collapsed;
@@ -38,17 +41,26 @@ namespace Something
             TheGame.Background = brush;
         }
 
+        Branch br = new Branch();
         private void SkipCode_btn(object sender, RoutedEventArgs e)
         {
             if (SkipCode_txt.Text == "")
             {
-                MessageBox.Show(br.texxt);
+                System.Windows.MessageBox.Show(br.texxt);
             }
             else
             {
-                MessageBox.Show($"This Feature is yet to be Implemented. \nYou typed {SkipCode_txt.Text}");
-
+                System.Windows.MessageBox.Show($"This Feature is yet to be Implemented. \nYou typed {SkipCode_txt.Text}");
             }
+        }
+        
+        private void Form1_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            double xp = e.GetPosition(centerPoint).X;
+            double yp = e.GetPosition(centerPoint).Y;
+            mouse.Text = $"({xp}, {yp})";
+            double hue = (Math.Atan2(xp,yp) * 180 / Math.PI) + 180;
+            OpeningScreen.Background = new SolidColorBrush(Branch.SimpleColorTransforms.HsLtoRgb(hue,0.7,0.5));
         }
     }
 }
--- Something/branch.cs
+++ Something/branch.cs
@@ -3,11 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using SystemMath = System.Math;
 
 namespace Something
 {
-    class branch
+    class Branch
     {
         public string texxt = "Blah";
+
+
+        public static class SimpleColorTransforms
+        {
+            private static double tolerance => 0.000000000000001;
+
+            public static Color HsLtoRgb(double h, double s, double l)
+            {
+                h = SystemMath.Max(0D, SystemMath.Min(360D, h));
+                s = SystemMath.Max(0D, SystemMath.Min(1D, s));
+                l = SystemMath.Max(0D, SystemMath.Min(1D, l));
+
+                // achromatic argb (gray scale)
+                if (SystemMath.Abs(s) < SimpleColorTransforms.tolerance)
+                {
+                    return Color.FromRgb(
+                            (byte)SystemMath.Max(0, SystemMath.Min(255, Convert.ToInt32(double.Parse($"{l * 255D:0.00}")))),
+                            (byte)SystemMath.Max(0, SystemMath.Min(255, Convert.ToInt32(double.Parse($"{l * 255D:0.00}")))),
+                            (byte)SystemMath.Max(0, SystemMath.Min(255, Convert.ToInt32(double.Parse($"{l * 255D:0.00}")))));
+                }
+
+                double q = l < .5D
+                        ? l * (1D + s)
+                        : (l + s) - (l * s);
+                double p = (2D * l) - q;
+
+                double hk = h / 360D;
+                double[] T = new double[3];
+                T[0] = hk + (1D / 3D); // Tr
+                T[1] = hk; // Tb
+                T[2] = hk - (1D / 3D); // Tg
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (T[i] < 0D)
+                        T[i] += 1D;
+                    if (T[i] > 1D)
+                        T[i] -= 1D;
+
+                    if ((T[i] * 6D) < 1D)
+                        T[i] = p + ((q - p) * 6D * T[i]);
+                    else if ((T[i] * 2D) < 1)
+                        T[i] = q;
+                    else if ((T[i] * 3D) < 2)
+                        T[i] = p + ((q - p) * ((2D / 3D) - T[i]) * 6D);
+                    else
+                        T[i] = p;
+                }
+
+                return Color.FromRgb(
+                        (byte)SystemMath.Max(0, SystemMath.Min(255, Convert.ToInt32(double.Parse($"{T[0] * 255D:0.00}")))),
+                        (byte)SystemMath.Max(0, SystemMath.Min(255, Convert.ToInt32(double.Parse($"{T[1] * 255D:0.00}")))),
+                        (byte)SystemMath.Max(0, SystemMath.Min(255, Convert.ToInt32(double.Parse($"{T[2] * 255D:0.00}")))));
+            }
+        }
     }
 }
